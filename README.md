4d2f66ac09698eda9eac9ddcb3ea72a490366134
Stephanie Vidor <stephanie.vidor@Stephanies-MacBook-Air.local>
2018-02-01 03:33:29 -0500

    My prework

---
+++ README.md
@@ -0,0 +1,3 @@
+I completed the pre work on a friends laptop, i am getting a new Mac.
+Bare with me
+I also joined the class really late.
--- TipTip/ViewController.swift
+++ TipTip/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var TotalLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    
+    @IBAction func onTap(_ sender: AnyObject) {
+        view.endEditing(true)
+    
+    }
+    @IBAction func CalculateTip(_ sender: AnyObject) {
+        
+        let tipPercentages = [0.18, 0.2, 0.25]
+       
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f" , tip)
+        TotalLabel.text = String(format: "$%.2f", total)
+    }
+    
 }
 
